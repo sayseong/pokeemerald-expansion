ee980e6505facae1cc591b7618bc3b5de933d35c
RoamerX <20692776+RoamerX@users.noreply.github.com>
2025-04-22 16:21:52 +0800

    继续检查每行字数超标，检查换行次数超标，汉化遗漏的英文文本

--- src/data/decoration/description.h
+++ src/data/decoration/description.h
@@ -4,17 +4,17 @@ const u8 DecorDesc_POKEMON_DESK[] = _("有著精灵球外形的小型桌子");
 
 const u8 DecorDesc_HEAVY_DESK[] = _("用铁做成的大型桌子，上面可放置东西");
 
-const u8 DecorDesc_RAGGED_DESK[] = _("用木头做成的大型桌子，上面可放置东西");
+const u8 DecorDesc_RAGGED_DESK[] = _("用木头做的大型桌子，上面可放置东西");
 
-const u8 DecorDesc_COMFORT_DESK[] = _("用叶子做成的大型桌子，上面可放置东西");
+const u8 DecorDesc_COMFORT_DESK[] = _("用叶子做的大型桌子，上面可放置东西");
 
-const u8 DecorDesc_PRETTY_DESK[] = _("用玻璃做成的巨大桌子，可放置许多东西");
+const u8 DecorDesc_PRETTY_DESK[] = _("用玻璃做的巨大桌子，可放置许多东西");
 
-const u8 DecorDesc_BRICK_DESK[] = _("用砖块做成的巨大桌子，可放置许多东西");
+const u8 DecorDesc_BRICK_DESK[] = _("用砖块做的巨大桌子，可放置许多东西");
 
-const u8 DecorDesc_CAMP_DESK[] = _("用圆木做成的巨大桌子，可放置许多东西");
+const u8 DecorDesc_CAMP_DESK[] = _("用圆木做的巨大桌子，可放置许多东西");
 
-const u8 DecorDesc_HARD_DESK[] = _("用石头做成的巨大桌子，可放置许多东西");
+const u8 DecorDesc_HARD_DESK[] = _("用石头做的巨大桌子，可放置许多东西");
 
 const u8 DecorDesc_SMALL_CHAIR[] = _("只能一个人坐的小椅子");
 
@@ -98,35 +98,35 @@ const u8 DecorDesc_JUMP_MAT[] = _("踩上去之后会跳跃的恶作剧地毯");
 
 const u8 DecorDesc_SPIN_MAT[] = _("踩上去之后会旋转的恶作剧地毯");
 
-const u8 DecorDesc_C_LOW_NOTE_MAT[] = _("踩上去后会发出低音Do声的地毯");
+const u8 DecorDesc_C_LOW_NOTE_MAT[] = _("踩上去会发出低音Do声的地毯");
 
-const u8 DecorDesc_D_NOTE_MAT[] = _("踩上去后会发出Re声的地毯");
+const u8 DecorDesc_D_NOTE_MAT[] = _("踩上去会发出Re声的地毯");
 
-const u8 DecorDesc_E_NOTE_MAT[] = _("踩上去后会发出Mi声的地毯");
+const u8 DecorDesc_E_NOTE_MAT[] = _("踩上去会发出Mi声的地毯");
 
-const u8 DecorDesc_F_NOTE_MAT[] = _("踩上去后会发出Fa声的地毯");
+const u8 DecorDesc_F_NOTE_MAT[] = _("踩上去会发出Fa声的地毯");
 
-const u8 DecorDesc_G_NOTE_MAT[] = _("踩上去后会发出So声的地毯");
+const u8 DecorDesc_G_NOTE_MAT[] = _("踩上去会发出So声的地毯");
 
-const u8 DecorDesc_A_NOTE_MAT[] = _("踩上去后会发出La声的地毯");
+const u8 DecorDesc_A_NOTE_MAT[] = _("踩上去会发出La声的地毯");
 
-const u8 DecorDesc_B_NOTE_MAT[] = _("踩上去后会发出Ti声的地毯");
+const u8 DecorDesc_B_NOTE_MAT[] = _("踩上去会发出Ti声的地毯");
 
-const u8 DecorDesc_C_HIGH_NOTE_MAT[] = _("踩上去后会发出高音Do声的地毯");
+const u8 DecorDesc_C_HIGH_NOTE_MAT[] = _("踩上去会发出高音Do声的地毯");
 
-const u8 DecorDesc_SURF_MAT[] = _("以冲浪形象制作的地毯，上面可放置东西");
+const u8 DecorDesc_SURF_MAT[] = _("冲浪形象的地毯，上面可放东西");
 
-const u8 DecorDesc_THUNDER_MAT[] = _("以打雷形象制作的地毯，上面可放置东西");
+const u8 DecorDesc_THUNDER_MAT[] = _("打雷形象的地毯，上面可放置东西");
 
-const u8 DecorDesc_FIRE_BLAST_MAT[] = _("以大字爆炎形象制作的地毯，上面可放置东西");
+const u8 DecorDesc_FIRE_BLAST_MAT[] = _("大字爆炎形象的地毯，上面可放置东西");
 
-const u8 DecorDesc_POWDER_SNOW_MAT[] = _("以细雪形象制作的地毯，上面可放置东西");
+const u8 DecorDesc_POWDER_SNOW_MAT[] = _("细雪形象的地毯，上面可放置东西");
 
-const u8 DecorDesc_ATTRACT_MAT[] = _("以迷人形象制作的地毯，上面可放置东西");
+const u8 DecorDesc_ATTRACT_MAT[] = _("迷人形象的地毯，上面可放置东西");
 
-const u8 DecorDesc_FISSURE_MAT[] = _("以地裂形象制作的地毯，上面可放置东西");
+const u8 DecorDesc_FISSURE_MAT[] = _("地裂形象的地毯，上面可放置东西");
 
-const u8 DecorDesc_SPIKES_MAT[] = _("以撒菱形象制作的地毯，上面可放置东西");
+const u8 DecorDesc_SPIKES_MAT[] = _("撒菱形象的地毯，上面可放置东西");
 
 const u8 DecorDesc_BALL_POSTER[] = _("印有精灵球的小张海报");
 
@@ -148,69 +148,69 @@ const u8 DecorDesc_SKY_POSTER[] = _("印有长翅鸥的大张海报");
 
 const u8 DecorDesc_KISS_POSTER[] = _("印有迷唇娃的大张海报");
 
-const u8 DecorDesc_PICHU_DOLL[] = _("皮丘的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_PICHU_DOLL[] = _("皮丘玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_PIKACHU_DOLL[] = _("皮卡丘的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_PIKACHU_DOLL[] = _("皮卡丘玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_MARILL_DOLL[] = _("玛力露的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_MARILL_DOLL[] = _("玛力露玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_TOGEPI_DOLL[] = _("波克比的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_TOGEPI_DOLL[] = _("波克比玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_CYNDAQUIL_DOLL[] = _("火球鼠的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_CYNDAQUIL_DOLL[] = _("火球鼠玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_CHIKORITA_DOLL[] = _("菊草叶的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_CHIKORITA_DOLL[] = _("菊草叶玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_TOTODILE_DOLL[] = _("小锯鳄的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_TOTODILE_DOLL[] = _("小锯鳄玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_JIGGLYPUFF_DOLL[] = _("胖丁的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_JIGGLYPUFF_DOLL[] = _("胖丁玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_MEOWTH_DOLL[] = _("喵喵的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_MEOWTH_DOLL[] = _("喵喵玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_CLEFAIRY_DOLL[] = _("皮皮的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_CLEFAIRY_DOLL[] = _("皮皮玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_DITTO_DOLL[] = _("百变怪的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_DITTO_DOLL[] = _("百变怪玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_SMOOCHUM_DOLL[] = _("迷唇娃的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_SMOOCHUM_DOLL[] = _("迷唇娃玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_TREECKO_DOLL[] = _("木守宫的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_TREECKO_DOLL[] = _("木守宫玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_TORCHIC_DOLL[] = _("火稚鸡的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_TORCHIC_DOLL[] = _("火稚鸡玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_MUDKIP_DOLL[] = _("水跃鱼的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_MUDKIP_DOLL[] = _("水跃鱼玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_DUSKULL_DOLL[] = _("夜巡灵的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_DUSKULL_DOLL[] = _("夜巡灵玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_WYNAUT_DOLL[] = _("小果然的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_WYNAUT_DOLL[] = _("小果然玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_BALTOY_DOLL[] = _("天秤偶的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_BALTOY_DOLL[] = _("天秤偶玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_KECLEON_DOLL[] = _("变隐龙的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_KECLEON_DOLL[] = _("变隐龙玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_AZURILL_DOLL[] = _("露力丽的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_AZURILL_DOLL[] = _("露力丽玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_SKITTY_DOLL[] = _("向尾喵的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_SKITTY_DOLL[] = _("向尾喵玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_SWABLU_DOLL[] = _("青绵鸟的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_SWABLU_DOLL[] = _("青绵鸟玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_GULPIN_DOLL[] = _("溶食兽的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_GULPIN_DOLL[] = _("溶食兽玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_LOTAD_DOLL[] = _("莲叶童子的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_LOTAD_DOLL[] = _("莲叶童子玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_SEEDOT_DOLL[] = _("橡实果的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_SEEDOT_DOLL[] = _("橡实果玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_PIKA_CUSHION[] = _("皮卡丘的垫子，要放置在地毯和桌子上");
+const u8 DecorDesc_PIKA_CUSHION[] = _("皮卡丘垫子，要放置在地毯和桌子上");
 
-const u8 DecorDesc_ROUND_CUSHION[] = _("玛力露丽的垫子，要放置在地毯和桌子上");
+const u8 DecorDesc_ROUND_CUSHION[] = _("玛力露丽垫子，要放置在地毯和桌子上");
 
-const u8 DecorDesc_KISS_CUSHION[] = _("迷唇娃的垫子，要放置在地毯和桌子上");
+const u8 DecorDesc_KISS_CUSHION[] = _("迷唇娃垫子，要放置在地毯和桌子上");
 
-const u8 DecorDesc_ZIGZAG_CUSHION[] = _("蛇纹熊的垫子，要放置在地毯和桌子上");
+const u8 DecorDesc_ZIGZAG_CUSHION[] = _("蛇纹熊垫子，要放置在地毯和桌子上");
 
-const u8 DecorDesc_SPIN_CUSHION[] = _("晃晃斑的垫子，要放置在地毯和桌子上");
+const u8 DecorDesc_SPIN_CUSHION[] = _("晃晃斑垫子，要放置在地毯和桌子上");
 
-const u8 DecorDesc_DIAMOND_CUSHION[] = _("勾魂眼的垫子，要放置在地毯和桌子上");
+const u8 DecorDesc_DIAMOND_CUSHION[] = _("勾魂眼垫子，要放置在地毯和桌子上");
 
-const u8 DecorDesc_BALL_CUSHION[] = _("精灵球的垫子，要放置在地毯和桌子上");
+const u8 DecorDesc_BALL_CUSHION[] = _("精灵球垫子，要放置在地毯和桌子上");
 
 const u8 DecorDesc_GRASS_CUSHION[] = _("草标志垫子，要放置在地毯和桌子上");
 
@@ -218,22 +218,22 @@ const u8 DecorDesc_FIRE_CUSHION[] = _("火标志垫子，要放置在地毯和�
 
 const u8 DecorDesc_WATER_CUSHION[] = _("水标志垫子，要放置在地毯和桌子上");
 
-const u8 DecorDesc_SNORLAX_DOLL[] = _("巨大的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_SNORLAX_DOLL[] = _("巨大玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_RHYDON_DOLL[] = _("巨大的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_RHYDON_DOLL[] = _("巨大玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_LAPRAS_DOLL[] = _("巨大的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_LAPRAS_DOLL[] = _("巨大玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_VENUSAUR_DOLL[] = _("巨大的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_VENUSAUR_DOLL[] = _("巨大玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_CHARIZARD_DOLL[] = _("巨大的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_CHARIZARD_DOLL[] = _("巨大玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_BLASTOISE_DOLL[] = _("巨大的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_BLASTOISE_DOLL[] = _("巨大玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_WAILMER_DOLL[] = _("巨大的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_WAILMER_DOLL[] = _("巨大玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_REGIROCK_DOLL[] = _("巨大的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_REGIROCK_DOLL[] = _("巨大玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_REGICE_DOLL[] = _("巨大的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_REGICE_DOLL[] = _("巨大玩偶，要放置在地毯和桌子上");
 
-const u8 DecorDesc_REGISTEEL_DOLL[] = _("巨大的玩偶，要放置在地毯和桌子上");
+const u8 DecorDesc_REGISTEEL_DOLL[] = _("巨大玩偶，要放置在地毯和桌子上");
