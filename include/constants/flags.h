5a019ec2cef88fe6c9c82d46bb5bad17a26a70ba
sayseong <70845428+sayseong@users.noreply.github.com>
2025-05-04 16:28:18 +0800

    dexnav 更新

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1222,9 +1222,9 @@
 #define FLAG_ITEM_SAFARI_ZONE_NORTH_EAST_NUGGET                     0x491
 #define FLAG_ITEM_SAFARI_ZONE_SOUTH_EAST_BIG_PEARL                  0x492
 
-#define FLAG_UNUSED_0x493                                           0x493 // Unused Flag
-#define FLAG_UNUSED_0x494                                           0x494 // Unused Flag
-#define FLAG_UNUSED_0x495                                           0x495 // Unused Flag
+#define FLAG_DEXNAV_SEARCHING                                        0x493 // DEXNAV FLAG
+#define FLAG_DEXNAV_DETECTOR_MODE                                    0x494 // DEXNAV FLAG
+#define FLAG_DEXNAV_GET                                           0x495 // 获得DEXNAV FLAG
 #define FLAG_UNUSED_0x496                                           0x496 // Unused Flag
 #define FLAG_UNUSED_0x497                                           0x497 // Unused Flag
 #define FLAG_UNUSED_0x498                                           0x498 // Unused Flag
