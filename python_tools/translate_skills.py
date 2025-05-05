5614d7b87fa5c4c9c36ffcf0a06a9eecba466c96
ExMingYan <2264568487@qq.com>
2025-05-05 16:37:20 +0800

    修改py文件中路径处理相关

--- python_tools/translate_skills.py
+++ python_tools/translate_skills.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python python3
 import os
 import re
 import pandas as pd
@@ -60,14 +61,13 @@ def replace_move_info(content, df):
     return content
 
 def log(message):
-    with open(current_folder+"\log.txt", "a", encoding="utf-8") as log_file:
+    with open(os.path.join(current_folder, "log.txt"), "a", encoding="utf-8") as log_file:
         log_file.write(message + "\n")
     print(message)
 
 if __name__ == "__main__":
-
-    work_file = current_folder +"\..\src\data\moves_info.h"
-    df = pd.read_excel(current_folder +r'\src\招式.xlsx')
+    work_file = os.path.join(os.path.dirname(current_folder), "src", "data", "moves_info.h")
+    df = pd.read_excel(os.path.join(current_folder, "src", "招式.xlsx"))
     df.set_index('招式', inplace=True)
 
 
